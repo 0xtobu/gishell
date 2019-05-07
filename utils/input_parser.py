fb45b8496830914ce5fd918b912cb791ccaf4e19
Tobias B <contact.me@tobu.tech>
2019-05-07 13:43:09 +0200

    Inital commit with argument parsing

---
+++ utils/input_parser.py
@@ -0,0 +1,18 @@
+import argparse
+
+def create_parser():
+    parse = argparse.ArgumentParser()
+
+    remote_settings = parse.add_argument_group('Remote host Settings')
+    remote_settings.add_argument('-rh','--RHOST', help='IP Address of Target Machine', dest="rhost")
+    remote_settings.add_argument('-rp','--RPORT', help='Port of Target Machine', dest="rport")
+
+    local_settings = parse.add_argument_group('Local host Settings')
+    local_settings = parse.add_argument('-lh','--LHOST', help='IP Address of your local machine', dest="lhost")
+    local_settings = parse.add_argument('-lp','--LPORT', help='Port of local machine.', dest="lport")
+
+    global_settings = parse.add_argument_group('Global application Settings')
+    global_settings.add_argument('-D', '--debug', default=False, action='store_true', help='Run in debug mode')
+    global_settings.add_argument('-p','--protocol', help='Protocol for packets, TCP or UDP' ,dest="protocol")
+    
+    return parse
